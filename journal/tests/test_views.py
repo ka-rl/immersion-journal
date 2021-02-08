56435e7656f8073825107123e8ae6b2ce7cc9616
ka-rl <48381834+ka-rl@users.noreply.github.com>
2021-02-08 14:52:26 +0100

    refactor test_views to use mock for faster testing

--- journal/tests/test_views.py
+++ journal/tests/test_views.py
@@ -17,26 +17,59 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'journal/home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'hours': '12', 'minutes': '10', 'category': 'active'})
-        self.assertEqual(Journal.objects.count(), 1)
-        new_item = Journal.objects.first()
-        self.assertEqual(new_item.hours, 12)
-        self.assertEqual(new_item.minutes, 10)
-        self.assertEqual(new_item.category, 'active')
-
-    def test_for_invalid_input_doesnt_save(self):
-        self.client.post('/', data={'hours': '00', 'minutes': '00', 'category': 'active'})
-        self.assertEqual(Journal.objects.count(), 0)
-
-    def test_redirects_after_POST_with_valid_data(self):
-        response = self.client.post('/', data={'hours': '12', 'minutes': '10', 'category': 'active'})
-        self.assertRedirects(response, '/')
-
-    def test_renders_home_page_after_POST_with_invalid_data(self):
-        response = self.client.post('/', data={'hours': '', 'minutes': '', 'category': 'active'})
-        self.assertTrue(response.status_code, 200)
-
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], JournalForm)
+
+    def test_for_invalid_input_doesnt_save_but_shows_error(self):
+        response = self.client.post('/', data={'hours': '00', 'minutes': '00', 'category': 'active'})
+        self.assertEqual(Journal.objects.count(), 0)
+        self.assertContains(response, 'Immersion can not be shorter than 1 minute')
+
+
+@patch('journal.views.JournalForm')
+class HomePageUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        self.request = HttpRequest()
+        self.request.POST['hours'] = '12'
+        self.request.POST['minutes'] = '12'
+        self.request.POST['category'] = '12'
+        self.request.user = Mock()
+
+    def test_passes_POST_data_to_JournalForm(self, mockJournalForm):
+        home_page(self.request)
+        mockJournalForm.assert_called_once_with(data=self.request.POST)
+
+    def test_does_not_save_if_form_invalid(self, mockJournalForm):
+        mock_form = mockJournalForm.return_value
+        mock_form.is_valid.return_value = False
+        home_page(self.request)
+        self.assertFalse(mock_form.save.called)
+
+    @patch('journal.views.redirect')
+    def test_redirects_to_form_returned_object_if_form_valid(
+            self, mock_redirect, mockJournalForm
+    ):
+        mock_form = mockJournalForm.return_value
+        mock_form.is_valid.return_value = True
+
+        response = home_page(self.request)
+
+        self.assertEqual(response, mock_redirect.return_value)
+        self.assertTrue(mock_form.save.called)
+
+    @patch('journal.views.render')
+    def test_renders_home_template_with_form_if_form_invalid(
+            self, mock_render, mockJournalForm
+    ):
+        mock_form = mockJournalForm.return_value
+        mock_form.is_valid.return_value = False
+
+        response = home_page(self.request)
+
+        self.assertEqual(response, mock_render.return_value)
+        mock_render.assert_called_once_with(
+            self.request, 'journal/home.html',
+            {'active': {'hours': '00', 'minutes': '00'}, 'passive': {'hours': '00', 'minutes': '00'}, 'form': mock_form}
+        )
